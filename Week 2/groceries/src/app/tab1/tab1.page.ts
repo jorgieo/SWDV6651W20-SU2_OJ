2192bdcd690785ca5cd6e11ae6f87186d6c91e25
jorgieo <52473778+jorgieo@users.noreply.github.com>
2020-07-15 19:31:21 -0400

    Added HTML template code. Added click event to buttons.

--- Week 2/groceries/src/app/tab1/tab1.page.ts
+++ Week 2/groceries/src/app/tab1/tab1.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  items = [
+    {
+      name:"Banana",
+      qty: 6
+    },
+    {
+      name:"Apple",
+      qty: 4
+    },    
+    {
+      name:"Milk",
+      qty: 1
+    },    
+    {
+      name:"Bread",
+      qty: 1
+    },    
+    {
+      name:"Eggs",
+      qty: 18
+    }
+  ];
 
+  deleteItem(){
+
+  };
+  
+  addItem(){
+
+  };
   constructor() {}
 
 }
